bcf85e161cabaaa5cc18d6a753370d401d45baa8
9-Rahul <86327028+9-Rahul@users.noreply.github.com>
2025-12-06 03:37:54 +0530

    Rahul dev (#1)
    * structure created
    
    * components added

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import ChatbotUi from './myComponents/chatbotInterface/ChatbotUi'
+import './globals.css'
+import './myComponents/chatbotInterface/ChatbotUi.css'
+
+export default function Home() {
+  return (
+    <main>
+      <ChatbotUi />
+    </main>
+  )
+}
