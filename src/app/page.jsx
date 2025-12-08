1f439a9a779b4b8e5484009e3aaf278fd294d302
19Kushagra0 <19Kushagra0@gmail.com>
2025-12-08 11:06:48 +0530

    chatBot ui  created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
-'use client'
+"use client";
 
-import ChatbotUi from './myComponents/chatbotInterface/ChatbotUi'
-import './globals.css'
-import './myComponents/chatbotInterface/ChatbotUi.css'
+import ChatbotUi from "./myComponents/chatbotInterface/ChatbotUi";
+import ChatUi from "./myComponents/chatUi/ChatUi";
+import "./globals.css";
+import "./myComponents/chatbotInterface/ChatbotUi.css";
 
 export default function Home() {
   return (
     <main>
-      <ChatbotUi />
+      {/* <ChatbotUi /> */}
+
+      <ChatUi />
     </main>
-  )
+  );
 }
